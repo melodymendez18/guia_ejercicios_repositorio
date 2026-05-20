dbfaefbe0304bf395b259be182ee5883fd3defea
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:36:44 -0600

    ejercicio_14
    Realice un formulario que permita ingresar dos números y envíe los datos a otro
    archivo PHP donde se muestre el resultado de la división.

---
+++ ej14.php
@@ -0,0 +1,6 @@
+<?php
+$metros = $_POST['metros'];
+$centimetros = $metros * 100;
+echo "<h2> Los metros a centímetros = $centimetros</h2>";
+echo "<br><a href='ej14.html'><button>Volver al formulario</button></a>";
+?>
