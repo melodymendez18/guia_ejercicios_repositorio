357f1c578158f1eeab8e4ecfb3526534c04f7741
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:06:20 -0600

    ejercicio_4
    Cree un formulario que solicite dos números y envíe los datos a otro archivo PHP
    donde se calcule y muestre la multiplicación.

---
+++ ej04.php
@@ -0,0 +1,7 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$resultado = $num1 * $num2;
+echo "<h2>Resultado de la multiplicación: $num1 x $num2 = $resultado</h2>";
+echo "<br><a href='ej04.html'><button>Volver al formulario</button></a>";
+?>
