c3f7c8561315d15e184ef6cf9950bc00303407d6
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 09:50:35 -0600

    index
    links de los ejercicios

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Menú Principal</title>
+</head>
+<body text="#fff" bgcolor="#2c3e50" text-decoration: none>
+  <center>
+    <h1>Menú de Ejercicios PHP</h1>
+    <a href="ej01.html"><button>Ejercicio 1  Bienvenida</button></a><br><br>
+    <a href="ej02.html"><button>Ejercicio 2  Suma</button></a><br><br>
+    <a href="ej03.html"><button>Ejercicio 3  Resta</button></a><br><br>
+    <a href="ej04.html"><button>Ejercicio 4  Multiplicación</button></a><br><br>
+    <a href="ej05.html"><button>Ejercicio 5  División</button></a><br><br>
+    <a href="ej06.html"><button>Ejercicio 6  Nombre y Edad</button></a><br><br>
+    <a href="ej07.html"><button>Ejercicio 7  Mayor de dos números</button></a><br><br>
+    <a href="ej08.html"><button>Ejercicio 8  Menor de dos números</button></a><br><br>
+    <a href="ej09.html"><button>Ejercicio 9  Par o Impar</button></a><br><br>
+    <a href="ej10.html"><button>Ejercicio 10  Promedio de notas</button></a><br><br>
+    <a href="ej11.html"><button>Ejercicio 11  Aprobado o Reprobado</button></a><br><br>
+    <a href="ej12.html"><button>Ejercicio 12  Área del Rectángulo</button></a><br><br>
+    <a href="ej13.html"><button>Ejercicio 13  Perímetro del Cuadrado</button></a><br><br>
+    <a href="ej14.html"><button>Ejercicio 14  Metros a Centímetros</button></a><br><br>
+    <a href="ej15.html"><button>Ejercicio 15  Celsius a Fahrenheit</button></a><br><br>
+    <a href="ej16.html"><button>Ejercicio 16  Salario del Empleado</button></a><br><br>
+    <a href="ej17.html"><button>Ejercicio 17  Descuento de Compra</button></a><br><br>
+    <a href="ej18.html"><button>Ejercicio 18  Factura</button></a><br><br>
+    <a href="ej19.html"><button>Ejercicio 19  Tabla de Multiplicar</button></a><br><br>
+    <a href="ej20.html"><button>Ejercicio 20  Suma del 1 al N</button></a><br><br>
+    <a href="ej21.html"><button>Ejercicio 21  Cantidad de Letras</button></a><br><br>
+    <a href="ej22.html"><button>Ejercicio 22  Dólares a Euros</button></a><br><br>
+    <a href="ej23.html"><button>Ejercicio 23  Precio con IVA</button></a><br><br>
+    <a href="ej24.html"><button>Ejercicio 24  Edad en 10 años</button></a><br><br>
+    <a href="ej25.html"><button>Ejercicio 25  Datos del Usuario</button></a><br><br>
+  </center>
+</body>
+</html>
