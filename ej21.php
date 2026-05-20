e9d48f06ff6b62f32c7babeebd75a866e6ecb778
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:45:28 -0600

    ejercicio_21
    Realice un formulario que permita ingresar dos números y envíe los datos a otro
    archivo PHP donde se muestre el resultado de la división.

---
+++ ej21.php
@@ -0,0 +1,6 @@
+<?php
+$palabra = $_POST['palabra'];
+$cantidad = strlen($palabra);
+echo "<h2>Cantidad de letras:  $cantidad </h2>";
+echo "<br><a href='ej21.html'><button>Volver al formulario</button></a>";
+?>
