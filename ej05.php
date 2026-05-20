2d6218593028e36802ebe33d453ca1c261c63fc6
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:09:18 -0600

    ejercicio_5
    Realice un formulario que permita ingresar dos números y envíe los datos a otro
    archivo PHP donde se muestre el resultado de la división.

---
+++ ej05.php
@@ -0,0 +1,7 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+  $resultado = $num1 / $num2;
+  echo "<h2>Resultado de la división: $num1 / $num2 = $resultado</h2>";
+echo "<br><a href='ej05.html'><button>Volver al formulario</button></a>";
+?>
