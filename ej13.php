3a7c37350cc2943ba4972b70c1154dfdab3db9ce
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:35:41 -0600

    Add files via upload
    Realice un formulario que permita ingresar dos números y envíe los datos a otro
    archivo PHP donde se muestre el resultado de la división.

---
+++ ej13.php
@@ -0,0 +1,6 @@
+<?php
+$lado = $_POST['lado'];
+$perimetro = $lado * 4;
+echo "<h2>Perímetro del cuadrado: $perimetro</h2>";
+echo "<br><a href='ej13.html'><button>Volver al formulario</button></a>";
+?>
