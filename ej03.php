7cbb776bbf234fec4dd0f7331523bbb9d9343b7c
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:04:07 -0600

    ejercicio_3
    Elabore un formulario que solicite dos números y que envíe los datos a otro archivo
    PHP donde se muestre el resultado de la suma.

---
+++ ej03.php
@@ -0,0 +1,7 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$resultado = $num1 - $num2;
+echo "<h2>Resultado de la resta: $num1 - $num2 = $resultado</h2>";
+echo "<br><a href='ej03.html'><button>Volver al formulario</button></a>";
+?>
