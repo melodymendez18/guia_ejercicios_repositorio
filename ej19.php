20c42734297b7fc164f676316b69a70620341246
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:42:47 -0600

    ejercicio_19
    Realice un formulario que permita ingresar dos números y envíe los datos a otro
    archivo PHP donde se muestre el resultado de la división.

---
+++ ej19.php
@@ -0,0 +1,15 @@
+<?php
+$n = $_POST['numero'];
+echo "<h2>Tabla de multiplicar del  $n </h2>";
+echo "<p> $n  x 1 = " . ($n * 1) . "</p>";
+echo "<p> $n  x 2 = " . ($n * 2) . "</p>";
+echo "<p> $n  x 3 = " . ($n * 3) . "</p>";
+echo "<p> $n  x 4 = " . ($n * 4) . "</p>";
+echo "<p> $n  x 5 = " . ($n * 5) . "</p>";
+echo "<p> $n  x 6 = " . ($n * 6) . "</p>";
+echo "<p> $n  x 7 = " . ($n * 7) . "</p>";
+echo "<p> $n  x 8 = " . ($n * 8) . "</p>";
+echo "<p> $n  x 9 = " . ($n * 9) . "</p>";
+echo "<p> $n  x 10 = " . ($n * 10) . "</p>";
+echo "<br><a href='ej19.html'><button>Volver al formulario</button></a>";
+?>
