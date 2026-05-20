5bb7db3a043b93700615d6df282da6dacd3d8c2c
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:49:14 -0600

    ejercicio_24
    Realice un formulario que permita ingresar dos números y envíe los datos a otro
    archivo PHP donde se muestre el resultado de la división.

---
+++ ej24.php
@@ -0,0 +1,6 @@
+<?php
+$edad = $_POST['edad'];
+$edad_futura = $edad + 10;
+echo "<h2>Dentro de 10 años tendrás: $edad_futura años</h2>";
+echo "<br><a href='ej24.html'><button>Volver al formulario</button></a>";
+?>
