8cc68b63c15048903736352420178f7d940fa007
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:34:34 -0600

    ejercicio_12
    Realice un formulario que permita ingresar dos números y envíe los datos a otro
    archivo PHP donde se muestre el resultado de la división.

---
+++ ej12.php
@@ -0,0 +1,7 @@
+<?php
+$base = $_POST['base'];
+$altura = $_POST['altura'];
+$area = $base * $altura;
+echo "<h2>Área del rectángulo: $area</h2>";
+echo "<br><a href='ej12.html'><button>Volver al formulario</button></a>";
+?>
