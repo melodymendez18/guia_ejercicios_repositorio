b15cf18a8353459c1d773290f37909715a0f7348
melodymendez18 <2686962@clases.edu.sv>
2026-05-20 10:10:46 -0600

    ejercicio_6
    Realice un formulario que permita ingresar dos números y envíe los datos a otro
    archivo PHP donde se muestre el resultado de la división.

---
+++ ej06.php
@@ -0,0 +1,7 @@
+<?php
+$nombre = $_POST['nombre'];
+$edad = $_POST['edad'];
+echo "<h2>Nombre: $nombre</h2>";
+echo "<h2>Edad: $edad años</h2>";
+echo "<br><a href='ej06.html'><button>Volver al formulario</button></a>";
+?>
